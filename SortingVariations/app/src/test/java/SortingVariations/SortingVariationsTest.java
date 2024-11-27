62a4ec40a0da39246c3a7a2aa7efab974001ee3c
frek <frek@itu.dk>
2024-11-27 17:28:46 +0100

    build gradle and mainclass running working

---
+++ SortingVariations/app/src/test/java/SortingVariations/SortingVariationsTest.java
@@ -0,0 +1,18 @@
+package SortingVariations;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SortingVariationsTest {
+
+    @Test
+    public void mergeSortTest() {
+        System.out.println("Testing");
+        assertTrue(true); // A simple test that should always pass
+    }
+
+    @Test
+    public void secondaryTest(){
+        assertTrue(true);
+    }
+}
