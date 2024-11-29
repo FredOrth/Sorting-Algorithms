1dff1a58346bf41235c4915a1b0ba3eac63b961f
frek <frek@itu.dk>
2024-11-29 16:07:54 +0100

    Initial mergeSort & build.gradle changes to run with ./gradlew and ./gradlew run -PMainclass=<package>.<MainFileName>

--- SortingVariations/app/src/main/java/SortingVariations/Main.java
+++ SortingVariations/app/src/main/java/SortingVariations/Main.java
@@ -7,5 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(new Main().getGreeting());
+        // assert false : "Assertions are enabled!";
+        // System.out.println("Assertions are disabled!");
+
+        Integer[] testingArray = new Integer[]{5,3,2,6,4,9,10,2,4};
+
+        MergeSort.sort(testingArray);
+        for (Integer integer : testingArray) {
+            System.out.println(integer);
+        }
     }
 }
--- SortingVariations/app/src/main/java/SortingVariations/mergeSort.java
+++ SortingVariations/app/src/main/java/SortingVariations/mergeSort.java
@@ -1,4 +1,62 @@
 package SortingVariations;
 
-public class mergeSort {
+
+
+public class MergeSort {
+
+    private MergeSort(){
+        // Class should not be instantiated
+    }
+
+
+    public static void sort(Comparable[] a, Comparable[] aux, int low, int high){
+        // Using Comparable we force the user to convert to complex types, should be fine
+        // Basically just a checker whether it's not valid. Or actually this is our base case?
+        if (high <= low) return;
+
+        // find mid
+        int mid = low + (high - low) / 2;
+
+        //recursively call sort of the low to mid and mid to high
+        sort(a, aux, low, mid);
+        sort(a, aux, mid + 1, high);
+
+        // final merge? or no, this is the merge for all the recursive calls when it becmes small enough
+        merge(a, aux, low, mid, high);
+    }
+
+    public static void sort(Comparable[] a){
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length-1);
+        //assert isSorted(a);
+    }
+
+    public static void merge(Comparable[] a, Comparable[] aux, int low, int mid, int high){
+
+        for (int k = low; k<=high; k++) {
+            aux[k] = a[k];
+        }
+
+
+        int i = low;
+        int j = mid+1;
+
+        for (int k = low; k <= high; k++) {
+            if      (i > mid)                   a[k] = aux[j++];
+            else if (j > high)                  a[k] = aux[i++];
+            else if (compare(aux[j], aux[i]))   a[k] = aux[j++];
+            else                                a[k] = aux[i++];
+            
+        }
+        
+    }
+
+    public static boolean compare(Comparable a, Comparable b){
+        int number = a.compareTo(b);
+        if (number <0) return true;
+        else return false;
+
+    }
+
+
 }
