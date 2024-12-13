a0b6277034a30e49555bfe63b481e15bb2275176
ANMV <145438734+AndreasVinkler@users.noreply.github.com>
2024-12-13 15:38:54 +0100

    IterativeMergeSortIndex (v.1) init

--- SortingVariations/app/src/main/java/SortingVariations/Main.java
+++ SortingVariations/app/src/main/java/SortingVariations/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         Integer[] arr2 = {11,5,22,23,24,25,1,6,5,4,3,9};
-        BinomialSort<Integer> bio = new BinomialSort<>(5, true);
+        IterativeMergeSortIndex<Integer> bio = new IterativeMergeSortIndex<>(3);
         bio.sort(arr2);
         System.out.println(Arrays.toString(arr2));
 
@@ -26,7 +26,7 @@ public class Main {
         StableTestClass str6 = new StableTestClass(1, 6);
         StableTestClass[] arr1 = {str,str1,str2,str3,str4,str5,str6};
 
-        IterativeMergeSortC<StableTestClass> po = new IterativeMergeSortC<>(1);
+        IterativeMergeSortIndex<StableTestClass> po = new IterativeMergeSortIndex<>(3);
         po.sort(arr1);
         for(int i = 0; i<arr1.length; i++){
             System.out.println(arr1[i].getComparable() + " " + arr1[i].getTester());
