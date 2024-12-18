f0423659491351ccd91b00a8c98b15450bb6eb7f
Mitartesio <tobiaslysdal@gmail.com>
2024-12-18 22:01:58 +0100

    Latest changes to gen plots + plots

--- generate_plots.py
+++ generate_plots.py
@@ -7,31 +7,58 @@ import pandas as pd
 # plt.savefig("test_plot.png")  # Save to a file to test non-GUI usage
 # plt.show()
 
-def generate_plot(csv_file: str, title: str, x_label: str, y_label: str):
+
+def generate_plot(
+    csv_file: str,
+    title: str,
+    x_label: str,
+    y_label: str,
+    firstGroup: str,
+    sndGroup: str,
+):
 
     data = pd.read_csv(csv_file)
     grouped = data.groupby("algorithm")
 
     plt.figure(figsize=(10, 6))
     for algorithm, group in grouped:
-        plt.plot(group["comparisons"], group["time"], label=algorithm, marker="o")
+        plt.plot(group[firstGroup], group[sndGroup], label=algorithm)
 
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
     plt.legend()
     plt.grid(True)
-    plt.savefig(f"{title}")
+    plt.savefig(f"{title}.png")
     plt.show()
 
 
 if __name__ == '__main__':
+    # generate_plot(
+    #     "resultsMergesort.csv",
+    #     "Mergesort Performance (Comparisons)",
+    #     "Number of Comparisons",
+    #     "Time (seconds)",
+    # )
+
     generate_plot(
-        "resultsMergesort.csv",
-        "Mergesort Performance (Comparisons)",
+        "resultsCutoffValues.csv",
+        "C vs comparisons",
+        "cutoff",
         "Number of Comparisons",
-        "Time (seconds)",
+        "cutoff",
+        "comparisons",
     )
+
+    generate_plot(
+        "resultsCutoffValues.csv",
+        "C vs time",
+        "cutoff",
+        "time",
+        "cutoff",
+        "time",
+    )
+
 
 # # Generate plots for Cutoff Values results
 # generate_plot(
