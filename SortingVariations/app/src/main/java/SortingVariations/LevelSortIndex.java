e946b048fc5e2c09d92eb8feecd242b8a2275a5f
Mitartesio <tobiaslysdal@gmail.com>
2025-02-19 11:58:06 +0100

    latest with proper lvl inheritance. Need to test and clean up types

--- SortingVariations/app/src/main/java/SortingVariations/LevelSortIndex.java
+++ SortingVariations/app/src/main/java/SortingVariations/LevelSortIndex.java
@@ -18,77 +18,45 @@ public class LevelSortIndex<T extends Comparable<T>> implements Sorter<T> {
         counter = 0;
 
         Stack<Integer[]> stack = new Stack<>();
-        Stack<integer[]> stackLvl = new Stack<>(); // This is where I think implementing a run class, that can have an attribute level.
+        Stack<int[]> stackLvl = new Stack<>(); // This is where I think implementing a run class, that can have an attribute level.
         int i = 0;
 
+        Integer[] run = findSequence(i, a);
         while (i < a.length) {
-            Integer[] run = new Integer[2];
+            Integer[] nextRun = new Integer[2];
+            nextRun[0] = i;
             run[0] = i;
 
-            // Determine run length
-            if (adaptive) {
-                int sequence = findSequence(i, a);
-                if (sequence >= cutoff) {
-                    run[1] = i + sequence - 1;
-                    i += sequence - 1;
-                } else {
-                    if (i + cutoff >= a.length) {
-                        run[1] = a.length - 1;
-                        insertionSort(a, i, run[1]);
-                        i = a.length;
-                    } else {
-                        run[1] = i + cutoff - 1;
-                        insertionSort(a, i, i + cutoff - 1);
-                        i += cutoff - 1;
-                    }
-                }
-            } else {
-                if (i + cutoff >= a.length) {
-                    run[1] = a.length - 1;
-                    insertionSort(a, i, run[1]);
-                    i = a.length;
-                } else {
-                    run[1] = i + cutoff - 1;
-                    insertionSort(a, i, i + cutoff - 1);
-                    i += cutoff - 1;
-                }
-            }
-            i++;
+            int k = validateSequence(nextRun, i, cutoff, adaptive);
+            int i += k;
 
-            // Merge runs from the stack
+            int lvl = computeLevel(int run[0], int run[1], nextRun[1]);
+
+            // Continue merging based on level size [run 1 on stack, run, nextRun]
             while (!stack.isEmpty()) {
-                Integer[] topRun = stack.peek();
-                if (topRun[1] - topRun[0] + 1 < run[1] - run[0] + 1) { //same as below.
-                    merge(a, aux, topRun[0], run[0] - 1, run[1]);
-                    stack.pop();
-                    run[0] = topRun[0];
+                int topRunLvl = stackLvl.peek();
+                if (topRunLvl <= lvl) { // i.e run1 (4) <= run(6)
+                    topRun = stack.pop(); //run 1
+                    topLvl = stackLvl.pop(); // 4
+                    merge(a, aux, run[0], run[0]-1, nextRun[1]); //run 1 and run
+                    run[0] = topRun[0]; // start of run = run1
+                    //topLvl = lvl // toplvl is now lvl otherwise recompute lvl of run1 + run to nextRun
+                    // just add lvl to lvlstack and throw toplvl out?
                 } else {
                     break;
                 }
             }
-
-            // Continue merging based on level size
-            while (!stack.isEmpty()) {
-                Integer[] topRun = stack.peek();
-                Integer[] topRunLvl = stack.peek();
-                if (topRun[1] - topRun[0] + 1 < (run[1] - run[0] + 1) * 2 && topRunLvl[0] <= topRunLvl[1]) { // this is the condition where we wanna incorporate the levelboundary calculation
-                    merge(a, aux, topRun[0], run[0] - 1, run[1]);
-                    run[0] = topRun[0];
-                    stack.pop();
-                } else {
-                    break;
-                }
-            }
-
-            assert stack.isEmpty() || stack.peek()[1] - stack.peek()[0] + 1 >= (run[1] - run[0] + 1) * 2;
+            //assert stack.isEmpty() || stack.peek()[1] - stack.peek()[0] + 1 >= (run[1] - run[0] + 1) * 2;
             stack.add(run);
+            //smarter way than this?
+            stackLvl.add(lvl);
+            run = nextRun;
         }
-
         // Final merging phase
         while (stack.size() > 1) {
-            Integer[] run = stack.pop();
-            merge(a, aux, stack.peek()[0], run[0] - 1, run[1]);
-            stack.peek()[1] = run[1];
+            Integer[] newRun = stack.pop();
+            merge(a, aux, stack.peek()[0], newRun[0]-1, newRun[1]);
+            stack.peek()[1] = newRun[1];
         }
         return counter;
     }
@@ -141,22 +109,55 @@ public class LevelSortIndex<T extends Comparable<T>> implements Sorter<T> {
 
     }
 
+    private int validateSequence(T[] run, int i, int cutoff, boolean adaptive) {
+        // Determine run length
+            if (adaptive) {
+                int sequence = findSequence(i, run);
+                if (sequence >= cutoff) {
+                    run[1] = i + sequence - 1;
+                    i += sequence - 1;
+                } else {
+                    if (i + cutoff >= a.length) {
+                        run[1] = a.length - 1;
+                        insertionSort(a, i, run[1]);
+                        i = a.length;
+                    } else {
+                        run[1] = i + cutoff - 1;
+                        insertionSort(a, i, i + cutoff - 1);
+                        i += cutoff - 1;
+                    }
+                }
+            } else {
+                if (i + cutoff >= a.length) {
+                    run[1] = a.length - 1;
+                    insertionSort(a, i, run[1]);
+                    i = a.length;
+                } else {
+                    run[1] = i + cutoff - 1;
+                    insertionSort(a, i, i + cutoff - 1);
+                    i += cutoff - 1;
+                }
+            }
+            i++;
+            return i;
+    }
 
-    private void computeLevel(int ia, int ib, int ic) {
+    private int computeLevel(int ia, int ib, int ic) {
         /* 
          * Takes 3 int arguments ia, ib and ic.
          * returns an int level
         */
-        long ml = (ia + (ib-1))/2
-        long mr = (ib + ic-1)/2
+        long ml = (ia + (ib-1))/2; // -1 or no?
+        long mr = (ib + ic-1)/2;
 
-        long xor = ml ^ mr // find the differing parts
+        long xor = ml ^ mr; // find the differing parts
 
-        int level = 64 - Long.numberOfLeadingZeros(xor)
+        int level = 64 - Long.numberOfLeadingZeros(xor);
 
-        return level
+        return level;
     }
 
+    //revert back to the low, mid, high policy from Binomial?
     private void merge(T[] a, T[] aux, int ia, int ib, int ic) { // if a[1, 2 ,3], then aux = [1, 2, 3]
         for (int k = ia; k <= ic; k++) {
             aux[k] = a[k]; // do the same for a and aux
